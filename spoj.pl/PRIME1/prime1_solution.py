5fab4423b0df5e1df7f091b7bf5cecbbbd4812d2
Vladimir Rutsky <altsysrq@gmail.com>
2011-04-11 10:24:42 +0400

    Use fast exponent

--- spoj.pl/PRIME1/prime1_solution.py
+++ spoj.pl/PRIME1/prime1_solution.py
@@ -4,6 +4,22 @@ import sys
 import random
 import math
 
+def exp_mod(a, b, n):
+    """Calculates  a^b mod n."""
+
+    a = a % n
+    # Loop invariant: 1 = a^b * res
+    # Loop ends when  a^b == 1  =>  res == a^b
+    res = 1
+    while b != 0:
+        if b % 2 == 1:
+            b = (b - 1)
+            res = (res * a) % n
+        else:
+            b /= 2
+            a = (a * a) % n
+    return res
+
 def is_prime(m, r):
     """Miller-Rabin primality test.
     See <https://secure.wikimedia.org/wikipedia/en/wiki/Miller%E2%80%93Rabin_primality_test>"""
@@ -26,11 +42,11 @@ def is_prime(m, r):
     for i in xrange(r):
         a = random.randint(2, m - 2)
 
-        x = a**t % m
+        x = exp_mod(a, t, m)
         if x == 1 or x == m - 1:
             continue
         for j in xrange(s - 1):
-            x = x**2 % m
+            x = exp_mod(x, 2, m)
             if x == 1:
                 return False
             elif x == m - 1:
