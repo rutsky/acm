1b50063d60a113561f348f5c59f5b249f70a541c
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-22 21:59:00 +0400

    FCTRL: Write code

--- spoj.pl/FCTRL/main.cpp
+++ spoj.pl/FCTRL/main.cpp
@@ -9,11 +9,54 @@
 
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+  vector<size_t> pows;
+  {
+    int pow = 5;
+    for (size_t i = 0; i < 14; ++i, pow *= 5)
+      pows.push_back(pow);
+  }
+
+  size_t  T;
+  assert(T >= 0);
+  cin >> T;
+  for (size_t idx = 0; idx < T; ++idx)
+  {
+    size_t N;
+    cin >> N;
+
+    assert(N >= 0);
+    if (N < 10)
+    {
+      cout << "0";
+    }
+    else
+    {
+      size_t const ns = N / 5;
+      
+      size_t idx(1), zeroes(0);
+      for (; idx < pows.size(); ++idx)
+      {
+        if (pows[idx] > N)
+        {
+          break;
+        }
+        else
+        {
+          zeroes += idx;
+        }
+      }
+      
+      cout << ns - idx + zeroes;
+    }
+
+    cout << "\n";
+  }
 }
 
 // vim: set ts=2 sw=2 et:
