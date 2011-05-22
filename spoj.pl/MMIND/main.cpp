24089815ed04e1578255ddc41dce4d2ef4f59729
Vladimir Rutsky <altsysrq@gmail.com>
2011-05-22 22:48:41 +0400

    MMIND: Add template (start readin problem)

---
+++ spoj.pl/MMIND/main.cpp
@@ -0,0 +1,17 @@
+/* main.cpp
+ * 12. The Game of Master-Mind.
+ * Vladimir Rutsky <altsysrq@gmail.com>
+ * 22.05.2011
+ *
+ */
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+}
+
+// vim: set ts=2 sw=2 et:
